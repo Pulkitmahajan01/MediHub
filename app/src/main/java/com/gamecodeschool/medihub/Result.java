c4ec1f0dff3ddf45d87f60117a3e17cd30624b38
Pulkitmahajan01 <pulkitmahajan.01@gmail.com>
2019-10-05 22:20:29 +0530

    uploading MediHub

---
+++ app/src/main/java/com/gamecodeschool/medihub/Result.java
@@ -0,0 +1,36 @@
+package com.gamecodeschool.medihub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Result extends AppCompatActivity {
+
+    private Button backButton;
+    private TextView resultTextView;
+    private String resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        resultTextView = findViewById(R.id.result_textview);
+        backButton = findViewById(R.id.back_button);
+        resultText = getIntent().getStringExtra(TextRecognization.RESULT_TEXT);
+
+        resultTextView.setText(resultText);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+}
